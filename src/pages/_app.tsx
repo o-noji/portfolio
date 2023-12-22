a6a8e49ee6051a63f594cecc0444bb60ac961c06
oの字 <107860356+o-noji@users.noreply.github.com>
2023-12-22 18:43:13 +0900

    ga4設定

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -26,6 +26,23 @@ function App({ Component, pageProps, router }: AppProps) {
   return (
     <MantineProvider>
       <Head>
+        {!!process.env.GA_MEASUREMENT_ID && (
+          <>
+            <script
+              async
+              src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_MEASUREMENT_ID}`}
+            ></script>
+            <script>
+              {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.GA_MEASUREMENT_ID}', { send_page_view: false });
+            `}
+            </script>
+          </>
+        )}
+
         <title>Onoji Portfolio</title>
         <meta name="description" content="岡田拓也のポートフォリオです！" />
         <meta name="robots" content="noindex" />
