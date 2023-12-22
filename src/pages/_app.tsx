1ecf53d68e9666b1bdad49f67e42854cc48409e0
oの字 <107860356+o-noji@users.noreply.github.com>
2023-12-22 19:00:32 +0900

    ページ遷移ごとにga4が機能するように修正

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import '@/styles/reset.css';
 import '@/styles/globals.scss';
 import '@mantine/core/styles.css';
 import type { AppProps } from 'next/app';
+import usePageTracking from '@/hooks/usePageTracking';
 import { MantineProvider } from '@mantine/core';
 import { AnimatePresence, motion } from 'framer-motion';
 import { useTransitionFix } from '@/utils/useTransitionFix';
@@ -22,27 +23,11 @@ const PAGE_VARIANTS = {
 };
 
 function App({ Component, pageProps, router }: AppProps) {
+  usePageTracking();
   const transitionCallback = useTransitionFix();
   return (
     <MantineProvider>
       <Head>
-        {!!process.env.GA_MEASUREMENT_ID && (
-          <>
-            <script
-              async
-              src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_MEASUREMENT_ID}`}
-            ></script>
-            <script>
-              {`
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${process.env.GA_MEASUREMENT_ID}', { send_page_view: false });
-            `}
-            </script>
-          </>
-        )}
-
         <title>Onoji Portfolio</title>
         <meta name="description" content="岡田拓也のポートフォリオです！" />
         <meta name="robots" content="noindex" />
