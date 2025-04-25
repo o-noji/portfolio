f8af638d07d66d427c3b5801ef99dbe909db0dfc
SJ-okada <okaden.denden@gmail.com>
2025-04-25 15:48:30 +0900

    修正

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,7 +5,7 @@ import '@mantine/core/styles.css';
 import type { AppProps } from 'next/app';
 import { MantineProvider } from '@mantine/core';
 import { AnimatePresence, motion } from 'framer-motion';
-import { useTransitionFix } from '@/utils/useTransitionFix';
+
 import Head from 'next/head';
 import Sidebar from '@/compornents/Sidebar';
 import ScriptGa from '@/hooks/ScriptGa';
@@ -23,7 +23,6 @@ const PAGE_VARIANTS = {
 };
 
 function App({ Component, pageProps, router }: AppProps) {
-  const transitionCallback = useTransitionFix();
   return (
     <MantineProvider>
       <Head>
@@ -35,7 +34,7 @@ function App({ Component, pageProps, router }: AppProps) {
       <Sidebar />
       <main className="main">
         <div className="main__inner">
-          <AnimatePresence mode="wait" onExitComplete={transitionCallback}>
+          <AnimatePresence mode="wait">
             <motion.div
               key={router.route}
               initial="initial"
